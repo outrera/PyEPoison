599f63bf5aa0ae28e33f2379cd5f84191a1d42ba
Yael Atletl <yael.atletl@gmail.com>
2017-06-17 00:09:23 -0500

    licenses

--- Active.py
+++ Active.py
@@ -1,27 +1,20 @@
 import psutil
 import subprocess
 import time
-from pathlib import Path
-#import gj_e3d_api
-args="tpack.exe"
-proc=subprocess.Popen(args)
-PID= proc.pid
-#my_file = Path("Closed.txt")
-try: #Intenta obtener el estado del proceso.
-	State = psutil.Process(PID)
-except: #Mensaje si no lo ha encontrado :c
-	print("Process couldn't be found")
-while psutil.pid_exists(PID): #Si el proceso es un zombie
-	if State.status: 
-			#keep_active()
-		
-		print(State.status)
-		print(PID)
-		time.sleep(3)
-	#if not State.status:
-	#	break
-
-
-#my_file = Path("Closed.txt")
-#if my_file.is_file():
-#if my_file.is_file():
+import gj_e3d_api
+import archivo_de_configuracion
+def Create_game_online(executable,
+def Create_game_offline(executable,level,var,bruh)
+	args=(executable+".exe -name Jugador -map "+level+" -variable Var1="+var+"-PrevMode")
+	proc=subprocess.Popen(args)
+	PID= proc.pid
+	try: #Intenta obtener el estado del proceso.
+		State = psutil.Process(PID)
+	except: #Mensaje si no lo ha encontrado :c
+		print("Process couldn't be found")
+	while psutil.pid_exists(PID): #Si el proceso es un zombie
+		if State.status: 
+			keep_active()
+			print(State.status)
+			print(PID)
+			time.sleep(3)
